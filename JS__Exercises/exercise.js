9bdb275249a486204f38d0b179ea9fd9da010c1b
kushal atreya <kushalatreya@live.com>
2018-09-17 14:18:28 +0300

    first commit

---
+++ JS__Exercises/exercise.js
@@ -0,0 +1,25 @@
+//“Facebook, Google, Microsoft, Apple, IBM,Oracle, Amazon” split at the string at the comma  and change it to an array.
+//Q3.i
+
+let companies = 'Facebook, Google, Microsoft, Apple, IBM,Oracle, Amazon'
+console.log(companies.split(" "))
+
+/* Array
+Declare an array  variable name itCompanies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon.
+Print the array using console.log()
+ Print the number of companies in the array
+Print out each company
+Capitalize each company and print them out
+Print the array like this: Facebook, Google, Microsoft, Apple, IBM,Oracle and Amazon are big IT companies.
+
+Sort the array using sort() method
+ */
+
+let itCompanies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+console.log(itCompanies);
+console.log(itCompanies.length);
+
+(itCompanies.forEach(function(element){
+    console.log(element);
+    
+}));
