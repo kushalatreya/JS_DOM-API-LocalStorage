81664a41a720e8036bac750980ac3e3272c64595
kushal atreya <kushalatreya@live.com>
2018-09-22 11:35:57 +0300

    daily  commit

--- DOM_Exercises/TableGenerator.js
+++ DOM_Exercises/TableGenerator.js
@@ -1,73 +1,94 @@
 console.log('Hello World, lets generate some table')
 
 //Dom Manipulation starts here:
-let wrapper = document.getElementsByTagName('body')[0];
+let wrapper = document.querySelector('.main-body');
+let table = document.createElement('table');
 
 function generate_table() {
-let mainTable = document.createElement('table');
+
+    table.innerHTML = ""; // to clear the table everytime, when pressed generate table.
+    let thead = document.createElement('thead');
+    let tbody = document.createElement('body');
+
+    let tableRow;
+    let tableHead;
+    let tableData;
+
+    let userInputRows = document.querySelector('.user-input-rows');
+    let genereatedRowsValue = userInputRows.value;
+
+    let userInputCol = document.querySelector('.user-input-col');
+    let genereatedColValue = userInputCol.value;
 
 
 
-wrapper.appendChild(mainTable)
+    
+    let userInputTableWidth = document.querySelector('.user-input-table-width');
+    let genereatedTableWidthValue = userInputTableWidth.value + '%';
+
+    let userInputBorderWidth = document.querySelector('.user-input-border-width');
+    let genereatedBorderWidth = userInputBorderWidth.value + "px";
+
+    let userInputTableColor = document.querySelector('.user-input-background-color')
+    let generatedTableColor = userInputTableColor.value
+
+    let userInputHeaderColor = document.querySelector('.user-input-header-color')
+    let generatedHeaderColor = userInputHeaderColor.value
+
+    let userInputBodyColor = document.querySelector('.user-input-body-color')
+    let generatedBodyColor = userInputBodyColor.value
+
+    let userInputBorderColor = document.querySelector('.user-input-border-color')
+    let generatedBorderColor = userInputBorderColor.value
+
+    let userInputFontColor = document.querySelector('.user-input-font-color')
+    let generatedFontColor = userInputFontColor.value
+
+    //let genereatedRowsValue = userInputRows.value;
+
+    table.width = genereatedTableWidthValue;
+    table.border = genereatedBorderWidth;
+    table.style.border = genereatedBorderWidth;
+
+    tbody.style.backgroundColor = generatedTableColor;
 
 
 
 
-}
 
+    for (let row = 0; row <= genereatedRowsValue; row++) {
+        tableRow = document.createElement('tr');
+        tableRow.textContent = row + 'row'
 
+        for (let col = 0; col <= genereatedColValue; col++) {
+            if (row === 0) {
+                tableHead = document.createElement('th');
+                //tableHead.style.width = '100%';
+                //tableData.style.width = '20px';
+                tableHead.textContent = col + 'hcel'
+                tableRow.appendChild(tableHead)
+                tbody.appendChild(tableRow)
+                tableHead.style.backgroundColor = generatedHeaderColor;
 
+            } else {
+                tableData = document.createElement('td');
+                tableData.textContent = col + "cell"
+                tableRow.appendChild(tableData)
+                tbody.appendChild(tableRow)
+            }
+        }
+        table.appendChild(thead);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* function generate_table() {
-//creating the body element of HTML as a wrapper (class name extracted from HTML)
-let wrapper = document.querySelector('.main-body');
-
-//creating Table to manupulate.
-let ourTable = document.createElement('table')
-
-let tableBody = document.createElement('tbody')
-
-for( let i=0; i < 2; i++){
-
-    let tableRow = document.createElement('tr');
-
-    for(let j = 0; j < 2; j++){
-        let tableData = document.createElement('
-        ');
-        let TableDataText = document.textContent("cell in row " + i + ", column " + j);
-
-        TableDataText.appendChild(tableData)
-        tableRow.appendChild(tableData)
+        wrapper.appendChild(table)
+        //table.setAttribute("border", "2px");
     }
-    tableBody.appendChild(tableRow)
-}
-//appending table to main body
+    table.appendChild(tbody);
 
-ourTable.appendChild(TableBody)
-
-wrapper.appendChild(ourTable)
-
- ourTable.setAttribute("border", "2");
+//styling
  
 
+
+
+
+
 }
- */
