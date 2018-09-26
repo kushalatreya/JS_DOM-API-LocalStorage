f0c4c0e286cf5124b4f8e3f6e159d59707891765
kushal atreya <kushalatreya@live.com>
2018-09-26 10:19:43 +0300

    Added sort functionalty and event listner

--- learning__API/countriesTable.js
+++ learning__API/countriesTable.js
@@ -1,30 +1,64 @@
-document.getElementById('getText').addEventListener('click', getTextFunction);
+//document.getElementById('getText').addEventListener('click', getCountriesListFunction);
 
 
 let divWrapper = document.querySelector('.mainDiv')
 let repeatingDiv = document.createElement('div');
 
-function getTextFunction() {
-
+function getCountriesListFunction() {
     //it clears page everytime this function is exicuted, so, the tabe of countries wouldn't repeat.
-    divWrapper.innerHTML = '';
     const url = 'https://restcountries.eu/rest/v2/all';
     fetch(url)
         .then(response => response.json())
         .then(countries => {
+            document.querySelector('.sort-countries-name').addEventListener('click', () => {
+                
+                showCountries(sortCountryName(countries));
+               
+            } );
+            document.querySelector('.reverse-countries-name').addEventListener('click', () => {
+
+               showCountries(sortCountryName(countries).reverse());
+
+            });
+            document.querySelector('.sort-capital-name').addEventListener('click', () => {
+
+            showCountries(sortCountryCapital(countries));
+
+            });
+            document.querySelector('.reverse-capital-name').addEventListener('click', () => {
+
+                showCountries(sortCountryCapital(countries).reverse());
+
+            });
+            document.querySelector('.sort-population-name').addEventListener('click', () => {
+                showCountries(sortPopulation(countries));
+            });
+            document.querySelector('.reverse-population-name').addEventListener('click', () => {
+                showCountries(sortPopulation(countries).reverse());
+            });
+            //sortPopulation(countries);
+            //sortCountryCapital(countries);
+            //sortCountryName(countries)
             showCountries(countries);
+
+            //forSelf: we create function to save the data from api in the memory location, 
         })
 }
+getCountriesListFunction()
 
 function showCountries(countries) {
+    //it clears page everytime this function is exicuted, so, the tabe of countries wouldn't repeat.
 
-     let countryName;
-     let countriesCapital;
-     let countriesPopulation;
-     let countriesFlagImage;
-    
+    divWrapper.innerHTML = '';
+    let countryName;
+    let countriesCapital;
+
+    let countriesPopulation;
+    let countriesRegion;
+    let countriesFlagImage;
+
+
     countries.forEach(country => {
-       
 
         repeatingDiv = document.createElement('div');
         repeatingDiv.className = 'repeatingDIV'
@@ -38,9 +72,12 @@ function showCountries(countries) {
         countriesPopulation = document.createElement('div')
         countriesPopulation.textContent = country.population;
 
+        countriesRegion = document.createElement('div')
+        countriesRegion.textContent = country.region;
+
         //creating an element 'div' which stores img later, it will be easier to style.
-        countriesFlag = document.createElement('div')
-         //creating an element 'img' instead of div, to store image
+        //countriesFlag = document.createElement('div')
+        //creating an element 'img' instead of div, to store image
         countriesFlagImage = document.createElement('img')
         countriesFlagImage.setAttribute('src', country.flag);
         countriesFlagImage.setAttribute("width", "200");
@@ -50,21 +87,56 @@ function showCountries(countries) {
         repeatingDiv.appendChild(countryName);
         repeatingDiv.appendChild(countriesCapital);
         repeatingDiv.appendChild(countriesPopulation);
-        countriesFlag.appendChild(countriesFlagImage);
-        repeatingDiv.appendChild(countriesFlag)
+        repeatingDiv.appendChild(countriesRegion);
+       // countriesFlag.appendChild(countriesFlagImage);
+        repeatingDiv.appendChild(countriesFlagImage);
+        //repeatingDiv.appendChild(countriesFlag)
         divWrapper.appendChild(repeatingDiv);
 
         //styling function is exicuted each time when the forEach runs.
         styling()
 
     })
+
 }
 
+
 function styling() {
-    repeatingDiv.style.backgroundColor = 'aliceBlue'
+   // repeatingDiv.style.backgroundColor = 'aliceBlue'
     repeatingDiv.style.border = 'solid'
     repeatingDiv.style.borderColor = 'skyBlue'
     repeatingDiv.style.borderWidth = '2px'
-    
-    
+}
+
+//forSelf: it is not possible to access the data directly, using console.log, we have to use loop to 
+//specify what exactly we are working .
+
+function sortPopulation(countries) {
+    const sortedPopulation = countries.sort((a, b) => a.population - b.population);
+    return sortedPopulation;
+}
+
+function sortCountryCapital(countries) {
+
+    const sortedByCapital = countries.sort((a, b) => {
+        if (a.capital > b.capital) {
+            return 1;
+        } else {
+            return -1
+        }
+    });
+    return sortedByCapital;
+}
+
+function sortCountryName(countries) {
+
+    const sortedByName = countries.sort((a, b) => {
+        console.log('a')
+        if (a.name > b.name) {
+            return 1;
+        } else {
+            return -1
+        }
+    });
+    return sortedByName;
 }
