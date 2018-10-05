0f2c00258c81e7294f9b7d4e52ca76396de27cd2
kushal atreya <kushalatreya@live.com>
2018-10-05 10:31:17 +0300

    refactoring code

--- learning__API/countriesTable.js
+++ learning__API/countriesTable.js
@@ -1,12 +1,8 @@
-//document.getElementById('getText').addEventListener('click', getCountriesListFunction);
-
-document.querySelector('.search-input').addEventListener('onkeydown', searchFunction )
 let divWrapper = document.querySelector('.mainDiv')
 let repeatingDiv = document.createElement('div');
-let searchInput = document.querySelector('.search-input');
+let inputValue = document.querySelector('.search-input');
 
 function getCountriesListFunction() {
-    //it clears page everytime this function is exicuted, so, the tabe of countries wouldn't repeat.
     const url = 'https://restcountries.eu/rest/v2/all';
     fetch(url)
         .then(response => response.json())
@@ -36,28 +32,25 @@ function getCountriesListFunction() {
             });
             document.querySelector('.reverse-population-name').addEventListener('click', () => {
                 showCountries(sortPopulation(countries).reverse());
-            });            
-            //sortPopulation(countries);
-            //sortCountryCapital(countries);
-            //sortCountryName(countries)
+            });
+            document.querySelector('.search-input').addEventListener("keyup", () => {
+                divWrapper.innerHTML = "";
+                Search(countries);
+            })
+
             showCountries(countries);
-            searchFunction(countries);
-            //forSelf: we create function to save the data from api in the memory location, 
         })
 }
 getCountriesListFunction()
 
 function showCountries(countries) {
-    //it clears page everytime this function is exicuted, so, the tabe of countries wouldn't repeat.
 
     divWrapper.innerHTML = '';
     let countryName;
     let countriesCapital;
-
     let countriesPopulation;
     let countriesRegion;
     let countriesFlagImage;
-
 
     countries.forEach(country => {
 
@@ -76,9 +69,6 @@ function showCountries(countries) {
         countriesRegion = document.createElement('div')
         countriesRegion.textContent = country.region;
 
-        //creating an element 'div' which stores img later, it will be easier to style.
-        //countriesFlag = document.createElement('div')
-        //creating an element 'img' instead of div, to store image
         countriesFlagImage = document.createElement('img')
         countriesFlagImage.setAttribute('src', country.flag);
         countriesFlagImage.setAttribute("width", "200");
@@ -89,9 +79,7 @@ function showCountries(countries) {
         repeatingDiv.appendChild(countriesCapital);
         repeatingDiv.appendChild(countriesPopulation);
         repeatingDiv.appendChild(countriesRegion);
-        // countriesFlag.appendChild(countriesFlagImage);
         repeatingDiv.appendChild(countriesFlagImage);
-        //repeatingDiv.appendChild(countriesFlag)
         divWrapper.appendChild(repeatingDiv);
 
         //styling function is exicuted each time when the forEach runs.
@@ -100,14 +88,10 @@ function showCountries(countries) {
 }
 
 function styling() {
-    // repeatingDiv.style.backgroundColor = 'aliceBlue'
     repeatingDiv.style.border = 'solid'
     repeatingDiv.style.borderColor = 'skyBlue'
     repeatingDiv.style.borderWidth = '2px'
 }
-
-//forSelf: it is not possible to access the data directly, using console.log, we have to use loop to 
-//specify what exactly we are working .
 
 function sortPopulation(countries) {
     const sortedPopulation = countries.sort((a, b) => a.population - b.population);
@@ -139,10 +123,11 @@ function sortCountryName(countries) {
     return sortedByName;
 }
 
-function searchFunction(countries){
-    const searchedElements = countries.filter((function(country){
-        (country.name = searchInput.value)
-        return true;
-    }));
-    console(searchedElements);
+function Search(countries) {
+
+    const results =
+        countries.filter(country => country.name.toLowerCase().startsWith(inputValue.value) ||
+            country.capital.toLowerCase().startsWith(inputValue.value) ||
+            country.region.toLowerCase().startsWith(inputValue.value));
+    showCountries(results);
 }
